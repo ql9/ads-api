52b9d5ad05d3ddf62eefd2acebdc5c3be95dfa83
Quốc Lê <ql9@MacBook-Pro.local>
2020-08-23 11:00:37 +0700

    update listen 5000 for heroku

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
+mongoose
+  .connect(dbConfig.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
+  });
+
+app.get("/", (req, res) => {
+  res.json({ message: "codebase ads-apu" });
 });
 
-
-app.get('/', (req, res) => {
-    res.json({"message": "codebase ads-apu"});
-});
-
-require('./app/routes/ads.routes.js')(app);
+require("./app/routes/ads.routes.js")(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is listening on port 5000");
 });
-
-
